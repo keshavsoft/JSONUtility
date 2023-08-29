a3862356290041fdcbd89074efee10fbc983ffe2
Keshav <nknnkumar@yahoo.com>
2023-08-29 11:23:37 +0530

    keshav

--- Projects/JSONUtility/Routes.js
+++ Projects/JSONUtility/Routes.js
@@ -1,10 +1,11 @@
 let express = require("express");
 let router = express.Router();
 
-let CommonMiddlewareJwtVerify = require("../../common/Jwt/Bs5")
+// let CommonMiddlewareJwtVerify = require("../../common/Jwt/Bs5");
+let CommonMiddlewareJwtVerify = require("../../common/Jwt/ForLogin/UserCredentials");
+
 let CommonUtility = require("./Routes/Utility")
 
 router.use("/Utility",CommonMiddlewareJwtVerify.ForKeshavSoftRedirectToLogin, CommonUtility);
-
 
 module.exports = router;
